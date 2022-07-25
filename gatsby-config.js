ce08967ac40917880e717c7215a2d3f866295ff6
trumgerpig <85285010+trumgerpig@users.noreply.github.com>
2022-07-25 15:05:38 -0400

    site url updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ try {
 
 if (
     process.env.NODE_ENV === `production` &&
-    config.siteUrl === `http://localhost:8000` &&
+    config.siteUrl === `https:blog.onioneducation.com` &&
     !process.env.SITEURL
 ) {
     throw new Error(
